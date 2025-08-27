9c58f20c7c704574013b13671464e8ccf635fb2f
mlam31 <mathieu.lam31@gmail.com>
2025-08-27 16:36:30 +0200

    ajout de struct Game et tests unitaires

--- src/bin/blackjack.rs
+++ src/bin/blackjack.rs
@@ -6,6 +6,21 @@ struct Card {
     value: Value,
     color: Color,
 }
+
+impl Card {
+    fn new(value: Value, color: Color) -> Self {
+        Self { value, color }
+    }
+
+    fn value(&self) -> &Value {
+        &self.value
+    }
+
+    fn color(&self) -> &Color {
+        &self.color
+    }
+}
+
 #[derive(Debug, Clone)]
 enum Color {
     Hearts,
@@ -69,17 +84,19 @@ impl Deck {
             cards: Vec::new(),
             length: 0,
         };
-        for color in colors {
-            for value in &values {
-                deck.cards.push(Card { value: value.clone(), color: color.clone() });
-                deck.length += 1;
+        for _ in 0..6 { // 6 jeux de 52 cartes
+            for color in &colors {
+                for value in &values {
+                    deck.cards.push(Card { value: value.clone(), color: color.clone() });
+                    deck.length += 1;
+                }
             }
         }
         deck
     }
 
     fn shuffle(&mut self) {
-        
+
         let mut rng = thread_rng();
         self.cards.shuffle(&mut rng);
     }
@@ -112,23 +129,150 @@ impl Hand {
     fn value(&self) -> u8 {
         let mut somme = self.cards.iter().map(|c| c.value.clone().to_int()).sum();
         let number_of_aces = self.cards.iter().filter(|c| c.value == Value::Ace).count();
-        if somme <= 11 && number_of_aces > 0 {
+        if somme <= 11 && number_of_aces > 0 { // Si la somme est inférieure ou égale à 11 et qu'il y a des As
             somme += 10;
         }
         somme
     }
 
-    fn get_value(&self) -> u8 {
-        self.value()
+    // Ajoute un paramètre is_dealer
+    fn show_value(&self, is_dealer: bool) -> u8 {
+        if is_dealer {
+            if let Some(card) = self.cards.first() {
+                let val = card.value.clone().to_int();
+                println!("{} + ?: [{:?} of {:?} + ?]", val, card.value, card.color);
+                val
+            } else {
+                println!("Main vide");
+                0
+            }
+        } else {
+            let val = self.value();
+            println!("{}: [{}]", val, self.cards.iter().map(|c| format!("{:?} of {:?}", c.value, c.color)).collect::<Vec<String>>().join(", "));
+            val
+        }
     }
 
     fn clear(&mut self) {
         self.cards.clear();
     }
 }
+#[derive(Debug)]
+enum Who {
+    Player,
+    Dealer,
+}
+#[derive(Debug)]
+struct Player {
+    hand: Hand,
+    money: u32,
+    bet: u32,
+    who: Who,
+}
+
+impl Player {
+    fn new(who: Who) -> Self {
+        Self {
+            hand: Hand::new(),
+            money: 1000,
+            bet: 10,
+            who,
+        }
+    }
+}
+#[derive(Debug)]
+struct Dealer {
+    hand: Hand,
+    bank: u32,
+}
+
+impl Dealer {
+    fn new() -> Self {
+        Self {
+            hand: Hand::new(),
+            bank: 100000,
+        }
+    }
+}
+#[derive(Debug)]
+struct Game {
+    deck: Deck,
+    players: Vec<Player>,
+    dealer: Dealer,
+}
+
+impl Game {
+    fn new() -> Self {
+        let deck = Deck::initialize_deck();
+        let players = Vec::new();
+        let dealer = Dealer {
+            hand: Hand::new(),
+            bank: 1000,
+        };
+        Self { deck, players, dealer }
+    }
+    fn deal_cards(&mut self) {
+        for _ in 0..2 {
+            for player in &mut self.players {
+                if let Some(card) = self.deck.draw() {
+                    player.hand.add_card(card);
+                }
+            }
+            if let Some(card) = self.deck.draw() {
+                self.dealer.hand.add_card(card);
+            }
+        }
+    }
+    fn show_hands(&self) {
+        for player in &self.players {
+            player.hand.show_value(false);
+        }
+        self.dealer.hand.show_value(true);
+    }
+}
 
 fn main() {
-    let mut deck = Deck::initialize_deck();
-    deck.shuffle();
-    println!("{:?}", deck.cards);
+    let mut player1 = Player::new(Who::Player);
+    let mut player2 = Player::new(Who::Player);
+    let mut game = Game::new();
+
+    game.deck.shuffle(); // Mélanger les cartes
+    game.players.push(player1); // Ajoute le joueur à la partie
+    game.players.push(player2); // Ajoute le deuxième joueur à la partie
+    game.deal_cards(); // Distribue les cartes
+    game.show_hands(); // Montre les mains des joueurs et du croupier
+    
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_deck_initialization() {
+        let deck = Deck::initialize_deck();
+        assert_eq!(deck.length, 312);
+    }
+
+    #[test]
+    fn test_ace_value() {
+        let ace_one = Card::new(Value::Ace, Color::Hearts);
+        let four = Card::new(Value::Four, Color::Diamonds);
+        let mut hand_one = Hand::new();
+        hand_one.add_card(ace_one);
+        hand_one.add_card(four);
+
+
+        let ace_ten = Card::new(Value::Ace, Color::Spades);
+        let nine = Card::new(Value::Nine, Color::Clubs);
+        let five = Card::new(Value::Five, Color::Hearts);
+        let mut hand_ten = Hand::new();
+        hand_ten.add_card(ace_ten);
+        hand_ten.add_card(nine);
+        hand_ten.add_card(five);
+
+        assert_eq!(hand_one.value(), 15);
+        assert_eq!(hand_ten.value(), 15);
+        
+    }
 }
